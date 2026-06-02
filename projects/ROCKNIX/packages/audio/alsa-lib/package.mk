12abbeb818d4d26bcbef3654dad59ba92792cfbb
zhangwei <zwdreams@gmail.com>
2026-06-02 20:59:12 +0800

    audio: update alsa stack, wireplumber and downgrade libvorbis for compatibility

--- projects/ROCKNIX/packages/audio/alsa-lib/package.mk
+++ projects/ROCKNIX/packages/audio/alsa-lib/package.mk
@@ -3,7 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-lib"
-PKG_VERSION="1.2.14"
+PKG_VERSION="1.2.16"
+PKG_SHA256="122b1e3166d55fe19bcde656535d7a36f2ab10e66c72c6ad2f43f20ffded0a96"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.alsa-project.org/"
 PKG_URL="https://www.alsa-project.org/files/pub/lib/alsa-lib-${PKG_VERSION}.tar.bz2"
