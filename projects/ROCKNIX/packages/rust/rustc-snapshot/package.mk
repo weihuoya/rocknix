9e6c5eb3fdd9f5cb1162e39b488123e64f96b87b
zhangwei <zwdreams@gmail.com>
2026-07-22 11:30:49 +0800

    update

--- projects/ROCKNIX/packages/rust/rustc-snapshot/package.mk
+++ projects/ROCKNIX/packages/rust/rustc-snapshot/package.mk
@@ -10,16 +10,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${MACHINE_HARDWARE_NAME}" in
   "aarch64")
-    PKG_SHA256="fb4ff31b71d7a2b6a793fb88fac18429befe807ddab3472bf87cbf92b6e5fbf6"
-    PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
+    PKG_SHA256="f91c23ade7e7b4ac173f12593eb1dcf1a37189d4e545ca2f64e3c14090ff6c0c"
+    PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="9228f0a8a00a75fe43f3c80fc3f8d73431aa75535bdd59a2dfdf704a7b9f6031"
-    PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.xz"
+    PKG_SHA256="1f67c31c3bc7abaa0212adb019e7e09a3963184ce1311479b9046b80745c5e1d"
+    PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="0a6b16ca476461c6238f48efcb74a7aa08a3983e61adf587f208ba48504d87e1"
-    PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
+    PKG_SHA256="ca0439140d02e91420f4755cc4681a6444a2dbe8e9a6f685f403946ed3efd995"
+    PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.gz"
     ;;
 esac
-PKG_SOURCE_NAME="rustc-snapshot_${PKG_VERSION}_${ARCH}.tar.xz"
+PKG_SOURCE_NAME="rustc-snapshot_${PKG_VERSION}_${ARCH}.tar.gz"
