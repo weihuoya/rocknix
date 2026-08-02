c351a44dbca2643bb6bbacc7993310923645933e
zhang wei <zwdreams@gmail.com>
2026-08-02 11:03:03 +0800

    downgrade libadwaita to 1.8.6 for glib 2.85.1 compatibility

--- packages/graphics/libadwaita/package.mk
+++ packages/graphics/libadwaita/package.mk
@@ -2,19 +2,18 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="libadwaita"
-PKG_VERSION="1.9.2"
-PKG_SHA256="8fc4d558acb10e237cdeb1b25247f0559c59956b99a7c1d41c1dc786fa21c953"
+PKG_VERSION="1.8.6"
+PKG_SHA256="87ece2f8d8ac4043766a7b87697e1f2b6925bbe3560e6f915066f9c88a6b9827"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://gitlab.gnome.org/GNOME/libadwaita"
 PKG_URL="https://gitlab.gnome.org/GNOME/libadwaita/-/archive/${PKG_VERSION}/libadwaita-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_DIR="libadwaita-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain gtk4 glib"
-PKG_LONGDESC="Building blocks for modern GNOME applications."
+PK_LONGDESC="Building blocks for modern GNOME applications."
 PKG_BUILD_FLAGS="-sysroot"
 
-PKG_MESON_OPTS_TARGET="-Dtests=false \
+PKG_MESON_OPTS_TARGET="-tests=false \
                        -Dvapi=false \
                        -Dintrospection=disabled \
                        -Dgtk_doc=false \
                        -Dexamples=false"
-
