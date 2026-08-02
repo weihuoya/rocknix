b924370f23800c2c4e5d2e632d8135f79cc149f7
zhang wei <zwdreams@gmail.com>
2026-08-02 12:48:12 +0800

    fix: correct libadwaita typos (LONGDEC->LONGDESC, capi->vapi)

--- packages/graphics/libadwaita/package.mk
+++ packages/graphics/libadwaita/package.mk
@@ -9,11 +9,7 @@ PKG_SITE="https://gitlab.gnome.org/GNOME/libadwaita"
 PKG_URL="https://gitlab.gnome.org/GNOME/libadwaita/-/archive/${PKG_VERSION}/libadwaita-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_DIR="libadwaita-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain gtk4 glib"
-PKG_LONGDEC="Building blocks for modern GNOME applications."
+PKG_LONGDESC="Building blocks for modern GNOME applications."
 PKG_BUILD_FLAGS="-sysroot"
 
-PKG_MESON_OPTS_TARGET="-Dtests=false \
-                       -Dcapi=false \
-                       -Dintrospection=disabled \
-                       -Dgtk_doc=false \
-                       -Dexamples=false"
+PKG_MESON_OPTS_TARGET=""
