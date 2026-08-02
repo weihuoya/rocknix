20f2382e1254c19a9dd2b070c08911bf3a1aafc1
zhang wei <zwdreams@gmail.com>
2026-08-02 14:43:11 +0800

    Downgrade libadwaita to 1.6.2 for glib 2.85 compatibility

--- packages/graphics/libadwaita/package.mk
+++ packages/graphics/libadwaita/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="libadwaita"
-PKG_VERSION="1.8.6"
-PKG_SHA256="87ece2f8d8ac4043766a7b87697e1f2b6925bbe3560e6f915066f9c88a6b9827"
+PKG_VERSION="1.6.2"
+PKG_SHA256="7542f8354e6808dd4e9a31551bbdfc0170735e4af4d1b3e69186500ccb9c01eb"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://gitlab.gnome.org/GNOME/libadwaita"
-PKG_URL="https://gitlab.gnome.org/GNOME/libadwaita/-/archive/${PKG_VERSION}/libadwaita-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://download.gnome.org/sources/libadwaita/1.6/libadwaita-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_DIR="libadwaita-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain gtk4 glib"
 PKG_LONGDESC="Building blocks for modern GNOME applications."
