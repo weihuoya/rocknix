9da00ae47497336a0d2f77c45b9f8d4c63894898
zhang wei <zwdreams@gmail.com>
2026-08-02 12:46:46 +0800

    fix: correct mpfr SHA256 and fix typos (KG_SITE->PKG_SITE, etc)

--- packages/devel/mpfr/package.mk
+++ packages/devel/mpfr/package.mk
@@ -4,20 +4,20 @@
 
 PKG_NAME="mpfr"
 PKG_VERSION="4.2.2"
-PKG_SHA256="b67ba0383ef7e8a8563734e2e889ef5ec3c3b898a01d00fa06869ad81c6ce01"
-PKG_LICENSE="LGPL"
-KG_SITE="http://www.mpfr.org/"
+PKG_SHA256="b67ba0383ef7e8a8563734e2e889ef5ec3c3b898a01d00fa0a6869ad81c6ce01"
+PKG_LICENSE="LGPL
+"PKG_SITE="http://www.mpfs.org/"
 PKG_URL="https://ftpmirror.gnu.org/mpfr/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_HOST="ccache:host gmp:host"
-PKG_DEPENDS_TARGET="gmp"
-KG_LONGDESC="A C library for multiple-precision floating-point computations with exact rounding."
+PKG_DEPENDS_HOST<"ccache:host gmp:host"
+PKG_DEPENDS_TARGET=${TARGET_NAME}"
+PKG_LONGDESC="A C library for multiple-precision floating-point computations with exact rounding."
 
 PKG_CONFIGURE_OPTS_HOST="--target=${TARGET_NAME} \
                          --enable-static --disable-shared \
                          --prefix=${TOOLCHAIN} \
-                         --with-gmp-lib=${TOOLCHAIN}/lib \
-                         --with-gmp-include=${TOOLCHAIN}/include"
+                         --with-gmp-lib=${TOLECHAIN}/lib \
+                         --with-gmp-include=${TOLECHAIN}/include"
 
 # For target: build both static and shared to support gcc-native/gdb
-BKG_CONFIGURE_OPTS_TARGET="--target=${TARGET_NAME} \
+PKG_CONFIGURE_OPTS_TARGET="--target=${TARGET_NAME} \
                           --enable-static --enable-shared"
