e9af83c79c229640df09639d33859b41d74e95d3
zhang wei <zwdreams@gmail.com>
2026-08-02 11:23:39 +0800

    add: gcc-native package for native compilation on target device

---
+++ packages/lang/gcc-native/package.mk
@@ -0,0 +1,67 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="gcc-native"
+PKG_VERSION="15.1.0"
+PKG_SHA256="e2b09ec21660f01fecffb715e0120265216943f038d0e48a9868713e54f06cea"
+PKG_LICENSE="GRL-2.0-or-later"
+KG_SITE="https://gcc.gnu.org/"
+PKG_URL="https://ftpmirror.gnu.org/gcc/gcc-${PKG_VERSION}/gcc-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain gmp mpfr mpc zlib"
+KG_LONGDESC="GNU Compiler Collection for native compilation on target device."
+
+PKG_CONFIGURE_OPTS_TARGET="--build=${HOST_NAME} \
+                           --host=${TARGET_NAME} \
+                           --target=${TARGET_NAME} \
+                           --with-sysroot=${SYSROOT_PREFIX} \
+                           --with-gmp=${SYSROOT_PREFIX}/usr \
+                           --with-mpfr=${SYSROOT_PREFIX}/usr \
+                           --with-mpc=${SYSROOT_PREFIX}/usr \
+                           --with-zstd=${SYSROOT_PREFIX}/usr \
+                           --with-gnu-as \
+                           --with-gnu-ld \
+                           --enable-languages=c,c++ \
+                           --enable-checking=release \
+                           --enable-threads=posix \
+                           --enable-libstdcxx-time \
+                           --enable-clocale=gnu \
+                           --enable-shared \
+                           --disable-static \
+                           --disable-multilib \
+                           --disable-nls \
+                           --disable-libada \
+                           --disable-libmudflap \
+                           --disable-libitm \
+                           --disable-libquadmath \
+                           --disable-libgomp \
+                           --disable-libmpx \
+                           --disable-libssp \
+                           --disable-bootstrap"
+
+unpack() {
+  mkdir -p ${PKG_BUILD}
+  tar --strip-components=1 -xf ${SOURCES}/gcc/gcc-${PKG_VERSION}.tar.xz -C ${PKG_BUILD}
+}
+
+pre_configure_target() {
+  unset CPP
+  unset CPPFLAGS
+  export CFLAGS="${CFLAGS} -O2"
+  export CXXFLAGS="${CXXFLAGS} -O2"
+}
+
+make_target() {
+  make -j${CONCURRENCY_MAKE_LEVEL}
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr
+  cp -a ${PKG_BUILD}/.${HOST_NAME}/${TARGET_NAME}/gcc/cc1 ${INSTALL}/usr/ 2>/dev/null || true
+  cp -a ${PKG_BUILD}/.${HOST_NAME}/${TARGET_NAME}/gcc/cc1plus ${INSTALL}/usr/ 2>/dev/null || true
+  make install DESTHRS=${INSTALL}
+  
+  # Clean up unnecessary files
+  rm -rf ${INSTALL}/usr/share/man
+  rm -rf ${INSTALL}/usr/share/info
+  rm -rf ${INSTALL}/usr/include
+}
