fa0948fba390b103668fee1c267c413adc0184cd
zhangwei <zwdreams@gmail.com>
2026-09-04 16:36:48 +0800

    workflow

---
+++ projects/ROCKNIX/packages/tools/neatvnc/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: ISC
+# Copyright (C) 2026-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="neatvnc"
+PKG_VERSION="af938aa6a39546df763af8865f6e25af1f0b26e9"
+PKG_GIT_CLONE_BRANCH="master"
+PKG_LICENSE="ISC"
+PKG_SITE="https://github.com/weihuoya/neatvnc"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain pixman aml libjpeg-turbo gnutls nettle gmp libdrm libpng zlib ffmpeg mesa"
+PKG_LONGDESC="A Neat VNC server library"
+GET_HANDLER_SUPPORT="git"
+
+PKG_MESON_OPTS_TARGET="-Dexamples=false \
+                       -Dbenchmarks=false \
+                       -Dtests=false \
+                       -Djpeg=enabled \
+                       -Dtls=enabled \
+                       -Dnettle=enabled \
+                       -Dgbm=enabled \
+                       -Dh264=enabled"
